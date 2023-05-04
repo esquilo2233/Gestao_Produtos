a3f278bf2ad39033f4276952e4e863901f062fa0
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-05-04 16:06:49 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Account a = new Account("cenas","123","cenas@op.pt")
+        Account a = new Account("cenas","123","cenas@op.pt");
     }
 }
