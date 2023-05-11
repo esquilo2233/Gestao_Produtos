e3e4028c71411b8e45ecb942e7f37f4f60e7247e
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-05-11 10:00:45 +0100

    ...

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Account a = new Account("cenas","123","cenas@op.pt");
-        User u =new User(a);
-        u.authenticate("cenas","123");
-        Address a1 =  new Address("c","b","o","4920-090");
+        //Account a = new Account("cenas","123","cenas@op.pt");
+        //User u =new User(a);
+        //u.authenticate("cenas","123");
+        //Address a1 =  new Address("c","b","o","4920-090");
+
     }
 }
